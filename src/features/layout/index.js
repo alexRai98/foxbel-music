5994cbec8ff393a056e35aeac195702c3b52040d
alexRai98 <raikcuerda19@gmail.com>
2020-11-07 17:36:25 -0500

    implement navba

--- src/features/layout/index.js
+++ src/features/layout/index.js
@@ -1,5 +1,5 @@
 /* @jsx jsx */
-// eslint-disable-next-line 
+// eslint-disable-next-line
 import { css, jsx } from '@emotion/core';
 import React, { Fragment } from 'react';
 import Menu from '../menu';
@@ -10,14 +10,7 @@ function Layout({ children }) {
     <Fragment>
       <Menu />
       <NavBar />
-      <div
-        css={css`
-          background-color: red;
-          width: 100%;
-        `}
-      >
-        {children}
-      </div>
+      {children}
     </Fragment>
   );
 }
